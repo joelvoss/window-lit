27b2ae54efff8d12adecaabddeeabcb3691b901d
Joel Voss <mail@joelvoss.com>
2021-02-17 22:17:57 +0100

    chore(tests): update unit tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -35,7 +35,7 @@ describe('useWindow', () => {
 									top: 0,
 									left: 0,
 									width: '100%',
-									height: '35px',
+									height: `${row.size}px`,
 									transform: `translateY(${row.start}px)`,
 								}}
 							>
@@ -47,9 +47,55 @@ describe('useWindow', () => {
 			);
 		}
 
-		
 		render(<App />);
 
 		expect(screen.getByText('Row 1')).toBeInTheDocument();
+	});
+
+	it('should render given dynamic size', async () => {
+		function App() {
+			const parentRef = React.useRef();
+
+			const { totalSize, virtualItems } = useWindow(parentRef, {
+				size: 10000,
+				overscan: 5,
+			});
+
+			return (
+				<div
+					ref={parentRef}
+					style={{ height: '200px', width: '200px', overflow: 'auto' }}
+				>
+					<div
+						style={{
+							position: 'relative',
+							width: '100%',
+							height: `${totalSize}px`,
+						}}
+					>
+						{virtualItems.map(row => (
+							<div
+								key={row.index}
+								ref={row.measureRef}
+								style={{
+									position: 'absolute',
+									top: 0,
+									left: 0,
+									width: '100%',
+									height: `${row.size}px`,
+									transform: `translateY(${row.start}px)`,
+								}}
+							>
+								Row {row.index}
+							</div>
+						))}
+					</div>
+				</div>
+			);
+		}
+
+		const rendered = render(<App />);
+
+		rendered.getByText('Row 1');
 	});
 });
