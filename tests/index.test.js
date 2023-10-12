22d8dd24f49537b2dd6973d6f1ebebbe817ae53e
Joel Voss <mail@joelvoss.com>
2023-10-12 20:47:41 +0200

    fix(test): fix imports in unit test

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 
 import { useWindow } from '../src/index';
 
